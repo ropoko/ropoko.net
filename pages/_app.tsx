cd301c3f1031b05198617a45c8c291ef847fcb14
ropoko <rodrigostramantinoli@gmail.com>
2021-06-15 23:16:51 -0300

    default document added and hello world in index.tsx working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> =  ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
 export default MyApp
