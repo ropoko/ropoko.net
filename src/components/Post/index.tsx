17f21c90886a7a17b38a34f57a4b6ab70c0fb7fe
ropoko <rodrigostramantinoli@gmail.com>
2021-07-22 21:34:51 -0300

    fix types on Post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
-import { ListPosts } from '../../pages'
+import { ListPosts } from '../../models/ListPosts'
 
-export default function Post({ post }: ListPosts) {
-  console.log(post)
-
+export default function Post({ post }: { post: ListPosts }) {
   return (
     <div>
-      <div>Posted on {post.frontmatter.date}</div>
-      <h3>{post.frontmatter.title}</h3>
+      <div>Posted on {post.date}</div>
+      <h3>{post.title}</h3>
 
       <Link href={`/posts/${post.slug}`}> Read more </Link>
     </div>
