f4fcd709d5d4621c01f78859b2a52915f493527f
ropoko <rodrigostramantinoli@gmail.com>
2022-06-16 00:58:15 -0300

    fix: font-family is loading correctly

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,8 +40,8 @@ export default class MyDocument extends Document {
 					/>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&
-						family=Rajdhani:wght@400;500;600&display=swap"
+						// eslint-disable-next-line max-len
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@400;500;600&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,5 +1,3 @@
-/* eslint @typescript-eslint/no-empty-interface: "off" */
-
 import 'styled-components';
 
 declare module 'styled-components' {
