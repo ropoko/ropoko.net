d405722bbd1e62fe3ae0baccbca7c63c63ee1384
ropoko <rodrigostramantinoli@gmail.com>
2023-01-09 11:36:04 -0300

    feat: added page about me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { AboutStyle } from '../styles/pages/about-style';
+import Link from 'next/link';
 
 const AboutPage = () => {
 	const img =
@@ -9,15 +10,40 @@ const AboutPage = () => {
 	return (
 		<AboutStyle>
 			<div className="column1">
-				<Image src={img} alt="Me" width="70" height="70" />
+				<Image src={img} alt="Me" width="200" height="200" />
+				<h3>You can reach me on</h3>
 				<div className="social-icons">
-					<Image src="/github.svg" alt="Me" width="24" height="24" />
-					<Image src="/linkedin.svg" alt="Me" width="24" height="24" />
-					<Image src="/twitter.svg" alt="Me" width="24" height="24" />
+					<Link href={'https://github.com/ropoko'}>
+						<a target="_blank">
+							<Image src="/github.svg" alt="Me" width="24" height="24" />
+						</a>
+					</Link>
+					<Link href={'https://www.linkedin.com/in/rodrigo-maganha/'}>
+						<a target="_blank">
+							<Image src="/linkedin.svg" alt="Me" width="24" height="24" />
+						</a>
+					</Link>
+					<Link href={'https://twitter.com/ropoko_'}>
+						<a target="_blank">
+							<Image src="/twitter.svg" alt="Me" width="24" height="24" />
+						</a>
+					</Link>
 				</div>
 			</div>
 			<div className="column2">
 				<h1>Howdy!</h1>
+				<h2>My name is Rodrigo!</h2>
+				<p>I'm a developer, interested in linux and open source in general.</p>
+				<p>In this blog you'll see stuff I've been working on or studying</p>
+				<p>
+					I'm currently working on{' '}
+					<Link href={'https://ropoko.itch.io/wild-league'}>Wild League</Link> a
+					real time strategy game using cards mechanics.
+				</p>
+				<p>
+					The game is been developed in Lua, the language I'm also currently
+					learning.
+				</p>
 			</div>
 		</AboutStyle>
 	);
